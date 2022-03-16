7569db2fbd5e5a21b92e2eab9f041378d604194d
Shourya Bansal <shouryab2001@gmail.com>
2022-03-16 13:05:34 +0530

    third commit : setup ejs and cookie parser

---
+++ config/mongoose.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/dailyFoodSubscription", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.set("useCreateIndex", true);
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "error coonecting to mongodb"));
+
+db.once("open", function () {
+  console.log("connected to database :: MongoDB");
+});
+
+module.exports = db;
--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
 const port = 8000;
+const expressLayouts = require("express-ejs-layouts");
+const db = require("./config/mongoose");
+const cookieParser = require("cookie-parser");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static("./assets"));
+
+app.use(expressLayouts);
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 app.use("/", require("./routes/index"));
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 app.listen(port, function (err) {
   if (err) {
