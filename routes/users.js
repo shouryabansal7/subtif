172c51aa5867b4be1436b2d6720b2caf46854f35
Shourya Bansal <shouryab2001@gmail.com>
2022-03-16 13:29:29 +0530

    create sign up and sign in

--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,10 @@ const router = express.Router();
 const userController = require("../controller/user_controller");
 
 router.get("/profile", userController.profile);
+router.get("/sign-up", userController.SignUp);
+router.get("/sign-in", userController.SignIn);
+
+router.post("/create", userController.create);
+router.post("/create-session", userController.createSession);
 
 module.exports = router;
