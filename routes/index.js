114c745d7cbf74abdfe9e957a155691ea67ef1b3
Shourya Bansal <shouryab2001@gmail.com>
2022-03-16 12:02:39 +0530

    second commit : create routes and controller

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const homeController = require("../controller/home_controller");
+
+router.get("/", homeController.home);
+router.use("/users", require("./users"));
+// router.use("/subscribe", require("./subscribe"));
+// router.use("/admin", require("./admin"));
+
+module.exports = router;
