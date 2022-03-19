44476716738397f598ff9d4a27e402685a891951
Shourya Bansal <shouryab2001@gmail.com>
2022-03-19 19:38:06 +0530

    tenth commit : create routes for admin

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,5 +1,52 @@
+const dashboard = require("../methods/dashboard");
+const leaves = require("../methods/leaves");
+const details = require("../methods/details");
+
 module.exports.home = function (req, res) {
   return res.render("home", {
     title: "Home",
   });
+};
+
+module.exports.forMonth = function (req, res) {
+  details.getHolidays({}).then((holidays) => {
+    dashboard.getUserData(req.user._id).then((user) => {
+      const month = dashboard.getMonth(req.params.month);
+      const unpaidAmount = details.getUnpaidAmount(user, month.value, holidays);
+      const totalAmount = unpaidAmount;
+      const leaveList = dashboard.getLeaveFormatted(user.leaves);
+      const monthList = dashboard.getMonthList();
+
+      // let lunchPrice =
+
+      res.render("dashboard", {
+        month: month,
+        // totalLunchPrice: totalLunchPrice,
+        // totalDinnerPrice: totalDinnerPrice,
+        totalAmount: totalAmount,
+        monthList: monthList,
+        leaveList: leaveList,
+      });
+    });
+  });
+};
+
+module.exports.monthLeaves = function (req, res) {
+  leaves.getUserData(req.user._id).then((user) => {
+    const monthList = leaves.getMonthList();
+    const month = leaves.getMonth(req.params.month);
+    const dateList = leaves.getDateList(month.value);
+    // console.log(dateList);
+    const leaveList = dashboard.getLeaveFormatted(user.leaves);
+    res.render("leaves", {
+      month: month,
+      monthList: monthList,
+      dateList: dateList,
+      leaveList: leaveList,
+    });
+  });
+};
+
+module.exports.submitLeaves = function (req, res) {
+  leaves.submitLeave(req, res, "/leaves");
 };
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 const homeController = require("../controller/home_controller");
+const { ensureAuthenticated, ensureGuest } = require("../helpers/auth");
 
-router.get("/", homeController.home);
+router.get("/", ensureGuest, homeController.home);
+router.post("/leaves", ensureAuthenticated, homeController.submitLeaves);
+router.get("/leaves/:month?", ensureAuthenticated, homeController.monthLeaves);
+router.get("/dashboard/:month?", ensureAuthenticated, homeController.forMonth);
 
 router.use("/users", require("./users"));
 router.use("/subscribe", require("./subscribe"));
+router.use("/admin", require("./admin"));
 // router.use("/subscribe", require("./subscribe"));
 // router.use("/admin", require("./admin"));
 
